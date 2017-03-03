8ad2640c82b3a903f1b6852a5eaeabbf24fee521
Nathan Hartley <nhartley@peckham.org>
2017-03-03 15:23:09 -0500

    Added license files.

--- PdfSharp.psm1
+++ PdfSharp.psm1
@@ -3,5 +3,5 @@ Set-StrictMode -Version Latest
 # Fail at the first exception.
 trap {}
 
-Add-Type -Path "$PSScriptRoot/inc/pdfsharp.dll"
+Add-Type -Path "$PSScriptRoot/PdfSharp/pdfsharp.dll"
 Get-ChildItem "$PSScriptRoot/inc/*.ps1" | foreach { . $_ }
