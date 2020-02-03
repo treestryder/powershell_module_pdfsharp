f9823594cb0952c1a2bab7f1726f0180e213c581
Nathan Hartley <nhartley@peckham.org>
2020-02-03 18:38:46 -0500

    Added Unprotect-Pdf to remove passwords.

---
+++ inc/Unprotect-Pdf.ps1
@@ -0,0 +1,47 @@
+function Unprotect-PDF {
+    <#
+     .Synopsis
+     Removes the password from a PDF file.
+    
+     .Parameter Path
+     Full path or array of PDF paths to decrypt. Excepts piped input. Required.
+    
+     .Example
+     Unprotect-PDF EncryptedFile.pdf
+    
+     .Example
+    Unprotect-PDF -Path encrypted.pdf
+     
+     .Example
+     dir *.pdf | Unprotect-PDF -Password password -Verbose
+    
+    #>
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory=$true,
+                ValueFromPipeline=$true,
+                ValueFromPipelineByPropertyName=$true,
+                HelpMessage='One or more PDF files to process')]
+            [ValidateNotNullorEmpty()]
+            [object[]]$Path,
+            [Parameter(Mandatory=$true,
+                HelpMessage='The password to decrypt the PDF files')]
+            [ValidateNotNullorEmpty()]
+            [string]$Password
+        )
+
+        process {
+            foreach ($file in $Path) {
+                if ($file -is [System.IO.FileInfo]) {$file = $file.FullName}
+                if ($file -isnot [string]) { continue }
+                Write-Verbose $file
+                try {
+                    $document = [PdfSharp.Pdf.IO.PdfReader]::Open($file, $password)
+                    $document.Save($file)
+                }
+                catch {
+                    throw $_
+                }
+            }
+        }
+    }
